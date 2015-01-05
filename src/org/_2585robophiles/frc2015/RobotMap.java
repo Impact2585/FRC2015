31efc74256cef0cb52d0a9956401a532012f3060
Amanuel Bayu <amanuelbayu@gmail.com>
2015-01-05 14:43:33 -0600

    Update RobotMap.java

--- src/org/_2585robophiles/frc2015/RobotMap.java
+++ src/org/_2585robophiles/frc2015/RobotMap.java
@@ -6,10 +6,10 @@ package org._2585robophiles.frc2015;
 public interface RobotMap {
 
 	public static final int FRONT_LEFT_DRIVE = -3,
-							FRONT_RIGHT_DRIVE = -3,
-							REAR_LEFT_DRIVE = -3,
-							REAR_RIGHT_DRIVE = -3,
-							SIDEWAYS_DRIVE = -3;
+				FRONT_RIGHT_DRIVE = -3,
+				REAR_LEFT_DRIVE = -3,
+				REAR_RIGHT_DRIVE = -3,
+				SIDEWAYS_DRIVE = -3;
 	
 	public static final double FORWARD_RAMPING = 1;
 }
