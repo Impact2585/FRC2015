ec0c4b51b108cae6d977255777e874081344af68
Killin-A13 <amanuelbayu@gmail.com>
2015-01-05 23:48:51 -0600

    add license

--- src/org/_2585robophiles/frc2015/RobotMap.java
+++ src/org/_2585robophiles/frc2015/RobotMap.java
@@ -1,15 +1,15 @@
 package org._2585robophiles.frc2015;
 
 /**
- * This interface contains the constants
+ * This interface contains the constants such as port numbers
  */
 public interface RobotMap {
 
 	public static final int FRONT_LEFT_DRIVE = -3,
-				FRONT_RIGHT_DRIVE = -3,
-				REAR_LEFT_DRIVE = -3,
-				REAR_RIGHT_DRIVE = -3,
-				SIDEWAYS_DRIVE = -3;
+							FRONT_RIGHT_DRIVE = -3,
+							REAR_LEFT_DRIVE = -3,
+							REAR_RIGHT_DRIVE = -3,
+							SIDEWAYS_DRIVE = -3;
 	
 	public static final double FORWARD_RAMPING = 1;
 }
