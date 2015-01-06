c5ba8d8dca0b8ea75fc33aae56cdfc8796c89c99
LoadingPleaseWait <michael@murphey.org>
2015-01-05 22:22:33 -0600

    trim down frcsim-installer.sh

--- frcsim-installer.sh
+++ frcsim-installer.sh
@@ -2,12 +2,7 @@
 
 # This is a modified FRCSim installer to get the dependencies for Travis CI
 
-# Add Gazebo Repository and Key
-sudo echo "deb http://packages.osrfoundation.org/gazebo/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-latest.list
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-
 # Add FRCSim Repository and Key
-sudo echo "deb http://users.wpi.edu/~adhenning/frcsim `lsb_release -cs` main" > /etc/apt/sources.list.d/frcsim-latest.list
 wget users.wpi.edu/~adhenning/frcsim.key -O - | sudo apt-key add -
 
 # Update and install frcsim and its dependencies
