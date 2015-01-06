6a5aa65919a44a177543f88e0314c9e88a927f2e
LoadingPleaseWait <michael@murphey.org>
2015-01-05 22:51:55 -0600

    use tee command and install gazebo

--- frcsim-installer.sh
+++ frcsim-installer.sh
@@ -2,10 +2,14 @@
 
 # This is a modified FRCSim installer to get the dependencies for Travis CI
 
+# Add Gazebo Repository and Key
+echo "deb http://packages.osrfoundation.org/gazebo/ubuntu `lsb_release -cs` main" | tee -a /etc/apt/sources.list.d/gazebo-latest.list
+wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
+
 # Add FRCSim Repository and Key
 echo "deb http://users.wpi.edu/~adhenning/frcsim `lsb_release -cs` main" | tee -a /etc/apt/sources.list.d/frcsim-latest.list
 wget users.wpi.edu/~adhenning/frcsim.key -O - | apt-key add -
 
 # Update and install frcsim and its dependencies
 apt-get update
-apt-get install -y frcsim
+apt-get install frcsim
