b214ee148aa09f0225a9cb222f63a7342feb606b
LoadingPleaseWait <michael@murphey.org>
2015-01-07 06:57:25 -0600

    corrected indentation in Robot class and commenting in TeleopExecuter

--- src/org/_2585robophiles/frc2015/TeleopExecuter.java
+++ src/org/_2585robophiles/frc2015/TeleopExecuter.java
@@ -3,6 +3,9 @@ package org._2585robophiles.frc2015;
 import org._2585robophiles.lib2585.RunnableExecuter;
 
 
+/**
+ * Executer for teleop (user operated) mode
+ */
 public class TeleopExecuter extends RunnableExecuter implements Initializable {
 	
 	private static final long serialVersionUID = 2506244240272489185L;
@@ -25,7 +28,7 @@ public class TeleopExecuter extends RunnableExecuter implements Initializable {
 	/* (non-Javadoc)
 	 * @see org._2585robophiles.frc2015.Initializable#init(org._2585robophiles.frc2015.Environment)
 	 */
-	@SuppressWarnings("unchecked")// not using generics necessary for Lib2585 to remain compatible with Java ME
+	@SuppressWarnings("unchecked")// not using generics so Lib2585 can remain compatible with Java ME
 	@Override
 	public void init(Environment environment) {
 		getRunnables().add(environment.getWheelSystem());
