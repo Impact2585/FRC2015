877709dbe86f3cb3d944b51507bee79d013ad1eb
Killin-A13 <amanuelbayu@gmail.com>
2015-01-18 15:46:57 -0600

    unit tested sidewaysdrive

--- test/org/_2585robophiles/frc2015/tests/WheelSystemTest.java
+++ test/org/_2585robophiles/frc2015/tests/WheelSystemTest.java
@@ -15,8 +15,9 @@ import edu.wpi.first.wpilibj.Joystick;
 public class WheelSystemTest {
 
 	private TestWheelSystem wheelSystem;
-	private double currentNormalMovement, currentSidewaysMovement, currentRotation;
-	
+	private double currentForwardMovement, currentSidewaysMovement,
+			currentRotation;
+
 	/**
 	 * Set up the unit test
 	 */
@@ -32,22 +33,22 @@ public class WheelSystemTest {
 	public void testRun() {
 		// first make sure we aren't moving without input
 		Assert.assertEquals(0, currentSidewaysMovement, 0);
-		Assert.assertEquals(0, currentNormalMovement, 0);
+		Assert.assertEquals(0, currentForwardMovement, 0);
 		Assert.assertEquals(0, currentRotation, 0);
-		
-		// test ramping
+
+		// test forward ramping
 		wheelSystem.setInput(new InputMethod() {
-			
+
 			@Override
 			public double sidewaysMovement() {
 				return 0;
 			}
-			
+
 			@Override
 			public double rotation() {
 				return 0;
 			}
-			
+
 			@Override
 			public double forwardMovement() {
 				return 1;
@@ -61,19 +62,19 @@ public class WheelSystemTest {
 		wheelSystem.run();
 		Assert.assertEquals(0, currentRotation, 0);
 		Assert.assertEquals(0, currentSidewaysMovement, 0);
-		Assert.assertEquals(RobotMap.FORWARD_RAMPING, currentNormalMovement, 0);
+		Assert.assertEquals(RobotMap.FORWARD_RAMPING, currentForwardMovement, 0);
 		wheelSystem.setInput(new InputMethod() {
-			
+
 			@Override
 			public double sidewaysMovement() {
 				return 0;
 			}
-			
+
 			@Override
 			public double rotation() {
 				return 0;
 			}
-			
+
 			@Override
 			public double forwardMovement() {
 				return -1;
@@ -87,26 +88,82 @@ public class WheelSystemTest {
 		wheelSystem.run();
 		Assert.assertEquals(0, currentRotation, 0);
 		Assert.assertEquals(0, currentSidewaysMovement, 0);
-		Assert.assertEquals(RobotMap.FORWARD_RAMPING + (-1 - RobotMap.FORWARD_RAMPING) * RobotMap.FORWARD_RAMPING, currentNormalMovement , 0);
+		Assert.assertEquals(RobotMap.FORWARD_RAMPING
+				+ (-1 - RobotMap.FORWARD_RAMPING) * RobotMap.FORWARD_RAMPING,
+				currentForwardMovement, 0);
 		
+		// test sideways ramping
+				wheelSystem.setInput(new InputMethod() {
+
+					@Override
+					public double sidewaysMovement() {
+						return 1;
+					}
+
+					@Override
+					public double rotation() {
+						return 0;
+					}
+
+					@Override
+					public double forwardMovement() {
+						return 0;
+					}
+
+					@Override
+					public Joystick[] joysticks() {
+						return null;
+					}
+				});
+				wheelSystem.run();
+				Assert.assertEquals(0, currentRotation, 0);
+				Assert.assertEquals(RobotMap.SIDEWAYS_RAMPING, currentSidewaysMovement, 0);
+				wheelSystem.setInput(new InputMethod() {
+
+					@Override
+					public double sidewaysMovement() {
+						return -1;
+					}
+
+					@Override
+					public double rotation() {
+						return 0;
+					}
+
+					@Override
+					public double forwardMovement() {
+						return 0;
+					}
+
+					@Override
+					public Joystick[] joysticks() {
+						return null;
+					}
+				});
+				wheelSystem.run();
+				Assert.assertEquals(0, currentRotation, 0);
+				Assert.assertEquals(RobotMap.SIDEWAYS_RAMPING
+						+ (-1 - RobotMap.SIDEWAYS_RAMPING) * RobotMap.SIDEWAYS_RAMPING,
+						currentSidewaysMovement, 0);
+
 		// test squared rotation
 		wheelSystem.setInput(new InputMethod() {
-			
+
 			@Override
 			public double sidewaysMovement() {
 				return 0;
 			}
-			
+
 			@Override
 			public double rotation() {
 				return -1;
 			}
-			
+
 			@Override
 			public Joystick[] joysticks() {
 				return null;
 			}
-			
+
 			@Override
 			public double forwardMovement() {
 				return 0;
@@ -115,22 +172,22 @@ public class WheelSystemTest {
 		wheelSystem.run();
 		Assert.assertEquals(-1, currentRotation, 0);
 		wheelSystem.setInput(new InputMethod() {
-			
+
 			@Override
 			public double sidewaysMovement() {
 				return 0;
 			}
-			
+
 			@Override
 			public double rotation() {
 				return 0.5;
 			}
-			
+
 			@Override
 			public Joystick[] joysticks() {
 				return null;
 			}
-			
+
 			@Override
 			public double forwardMovement() {
 				return 0;
@@ -143,25 +200,32 @@ public class WheelSystemTest {
 	/**
 	 * WheelSystem subclass for the purpose of unit testing
 	 */
-	private class TestWheelSystem extends WheelSystem{
+	private class TestWheelSystem extends WheelSystem {
 
-		/* (non-Javadoc)
-		 * @see org._2585robophiles.frc2015.systems.WheelSystem#drive(double, double, double)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org._2585robophiles.frc2015.systems.WheelSystem#drive(double,
+		 * double, double)
 		 */
 		@Override
-		public void drive(double normalMovement, double sidewaysMovement, double rotation) {
-			currentNormalMovement = normalMovement;
+		public void drive(double normalMovement, double sidewaysMovement,
+				double rotation) {
+			currentForwardMovement = normalMovement;
 			currentSidewaysMovement = sidewaysMovement;
 			currentRotation = rotation;
 		}
 
-		/* (non-Javadoc)
-		 * @see org._2585robophiles.frc2015.systems.WheelSystem#setInput(org._2585robophiles.frc2015.input.InputMethod)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org._2585robophiles.frc2015.systems.WheelSystem#setInput(org.
+		 * _2585robophiles.frc2015.input.InputMethod)
 		 */
 		@Override
 		protected synchronized void setInput(InputMethod input) {
 			super.setInput(input);
 		}
-		
+
 	}
 }
