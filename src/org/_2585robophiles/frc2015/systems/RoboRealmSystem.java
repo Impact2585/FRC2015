bf0a91cce4bc2603f8a987afca5c3f1fbf9c1a6d
Gal Egozi <gegozi100@online.houstonisd.org>
2015-02-16 18:03:33 -0600

    added vision

--- src/org/_2585robophiles/frc2015/systems/RoboRealmSystem.java
+++ src/org/_2585robophiles/frc2015/systems/RoboRealmSystem.java
@@ -10,15 +10,19 @@ import edu.wpi.first.wpilibj.tables.ITableListener;
  * The robot vision is implemented in this class.
  */
 public class RoboRealmSystem implements RobotSystem, ITableListener {
-    
+    private final double MIN_AREA=0; //The value is 0 for testing purposes only.
+    private final double MAX_AREA=1; //The value is 1 for testing purposes only.
+    private double distance;
+    private final double DISTANCE_CONSTANT=0; //The value is 0 for testing purposes only.
+    private double area;
+    private double xCoord;
+	private double yCoord;
     private NetworkTable nt;
     
     /**
      * Nothing is done in this constructor
      */
-    public RoboRealmSystem()
-    {    
-    }
+    public RoboRealmSystem() {}
 
     /* (non-Javadoc)
      * @see org._2585robophiles.aerbot.systems.RobotSystem#init(org._2585robophiles.aerbot.Environment)
@@ -28,15 +32,14 @@ public class RoboRealmSystem implements RobotSystem, ITableListener {
     {
         nt = NetworkTable.getTable("VisionTable");
         nt.addTableListener(this);
+        setNums();
     }
 
     /* (non-Javadoc)
      * @see org._2585robophiles.aerbot.systems.RobotSystem#destroy()
      */
     @Override
-    public void destroy()
-    {
-    }
+    public void destroy() {}
 
     /* (non-Javadoc)
      * @see edu.wpi.first.wpilibj.tables.ITableListener#valueChanged(edu.wpi.first.wpilibj.tables.ITable, java.lang.String, java.lang.Object, boolean)
@@ -45,5 +48,17 @@ public class RoboRealmSystem implements RobotSystem, ITableListener {
     public void valueChanged(ITable itable, String key, Object obj, boolean isNew)
     {
         SmartDashboard.putString(key, obj.toString());
+        setNums();
     }
+    public void setNums()
+    {
+    	yCoord=nt.getNumber("COG_Y");
+    	xCoord=nt.getNumber("COG_X");
+    	area=nt.getNumber("COG_AREA");
+    	if(MIN_AREA<area&&area<MAX_AREA) {distance=area/DISTANCE_CONSTANT;}
+    	else {area=-5555;}
+    }
+    public double getDistance() {return distance;}
+    public double getX() {return xCoord;}
+    public double getY() {return yCoord;}
 }
